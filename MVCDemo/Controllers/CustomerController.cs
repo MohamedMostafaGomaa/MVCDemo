83b963e3861f04af67ea5bfab4d136c1eb23d216
Mohamed Mostafa Gomaa <Moh.MostafaGomaa@gmail.com>
2018-12-07 11:27:31 -0500

    add samples

---
+++ MVCDemo/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return Content(Server.HtmlEncode("Mohamed Gomaa"));
+           // return View();
+        }
+
+        [OutputCache(Duration = 15)]
+        public string GetAllCustomers()
+        {
+            return @"<ul>
+            <li>Ali Raza</li>
+            <li>Mark Upston</li>
+            <li>Allan Bommer</li>
+            <li>Greg Jerry</li>
+            </ul>";
+        }
+    }
+}
--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDemo.Filters;
 
 namespace MVCDemo.Controllers
 {
+    [MyLogActionFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -24,6 +26,53 @@ namespace MVCDemo.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+
+        public ActionResult GoToCustomersPage()
+        {
+            return RedirectToAction("GetAllCustomers", "Customer");
+        }
+
+        #region Selectors
+
+        /// <summary>
+        /// 
+        /// There are three types of action selector attributes −
+
+        ///  ActionName
+        ///  NonAction
+        ///  ActionVerbs
+        /// </summary>
+        /// <returns></returns>
+
+        [ActionName("CurrentTime")]
+        [OutputCache(Duration = 20)]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("T");
+        }
+
+        [NonAction]
+        public string TimeString()
+        {
+            return "Time is " + DateTime.Now.ToString("T");
+        }
+
+        /*
+         MVC framework supports the following ActionVerbs:
+            HttpGet
+            HttpPost
+            HttpPut
+            HttpDelete
+            HttpOptions
+            HttpPatch
+         */
+        #endregion
+
+        public ActionResult MyView()
+        {
             return View();
         }
     }
