7c2aa35a883c7d0cb06c82f4e8ccfe642514342f
Mohamed Mostafa Gomaa <Moh.MostafaGomaa@gmail.com>
2018-12-07 13:17:49 -0500

    add security refresh

---
+++ MVCSecurityDemo/Controllers/SecretController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSecurityDemo.Controllers
+{
+    [Authorize]
+    //[Authorize(Users = "ali.khan@outlook.com")]
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        // GET: Secret
+        public ContentResult Secret()
+        {
+            return Content("Secret informations here");
+        }
+
+        [AllowAnonymous]
+        public ContentResult PublicInfo()
+        {
+            return Content("Public informations here");
+        }
+    }
+}
